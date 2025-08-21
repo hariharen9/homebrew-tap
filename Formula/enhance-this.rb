4a2af212a7b2d083ca874e06fc17ffb8ce63c9f5
Hariharen <57080243+hariharen9@users.noreply.github.com>
2025-08-21 11:39:34 +0530

    Create enhance-this.rb

---
+++ Formula/enhance-this.rb
@@ -0,0 +1,21 @@
+# Formula/enhance-this.rb
+class EnhanceThis < Formula
+  include Language::Python::Virtualenv
+
+  desc "CLI tool to enhance prompts using Ollama AI"
+  homepage "https://github.com/hariharen9/enhance-this"
+  # url "https://pypi.io/packages/source/e/enhance-this/enhance-this-0.1.0.tar.gz" # <-- TODO: Update with real URL on first release
+  # sha256 "..." # <-- TODO: Update with real hash on first release
+  url "https://github.com/hariharen9/enhance-this/archive/v0.1.0.tar.gz" # Placeholder
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/enhance", "--version"
+  end
+end
