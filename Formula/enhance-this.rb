8a1a9c54da77a53574b482ba83e2d4f987605f30
Hariharen <57080243+hariharen9@users.noreply.github.com>
2025-09-01 13:20:06 +0530

    enhance-this 0.2.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/enhance-this.rb
+++ Formula/enhance-this.rb
@@ -6,8 +6,8 @@ class EnhanceThis < Formula
   homepage "https://github.com/hariharen9/enhance-this"
   # url "https://pypi.io/packages/source/e/enhance-this/enhance-this-0.1.0.tar.gz" # <-- TODO: Update with real URL on first release
   # sha256 "..." # <-- TODO: Update with real hash on first release
-  url "https://github.com/hariharen9/enhance-this/archive/refs/tags/v0.2.1.tar.gz" # Placeholder
-  sha256 "c37afa1b03b6987fa5fa8f719d15b9cef9128640e819721a95f8c7929c8d89ad" # Placeholder
+  url "https://github.com/hariharen9/enhance-this/archive/refs/tags/v0.2.2.tar.gz" # Placeholder
+  sha256 "e0442b6f4ab651dfb172d19d62bd4b461f19c3a429aa0706217e9573e4fb312c" # Placeholder
 
   depends_on "python@3.11"
 
